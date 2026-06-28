9d7fa1ce363319b2d594c485ae12db3b691126fb
rachelmarro <yajaira.marroquin@catolica.edu.sv>
2026-06-27 21:48:35 -0600

    Correccion de carruseles en celular

--- js/script.js
+++ js/script.js
@@ -3,31 +3,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
     carruseles.forEach((carrusel) => {
         const lista = carrusel.querySelector(".menu-lista");
-        const ventana = carrusel.querySelector(".carrusel-ventana");
         const izquierda = carrusel.querySelector(".flecha-izquierda");
         const derecha = carrusel.querySelector(".flecha-derecha");
-        const tarjeta = carrusel.querySelector(".tarjeta");
-
-        let posicion = 0;
-
-        derecha.addEventListener("click", () => {
-            const ancho = tarjeta.offsetWidth + 18;
-            const maxScroll = lista.scrollWidth - ventana.offsetWidth;
-
-            posicion += ancho;
-            if (posicion > maxScroll) posicion = 0;
-
-            lista.style.transform = `translateX(-${posicion}px)`;
-        });
 
         izquierda.addEventListener("click", () => {
-            const ancho = tarjeta.offsetWidth + 18;
-            const maxScroll = lista.scrollWidth - ventana.offsetWidth;
+            lista.scrollBy({
+                left: -280,
+                behavior: "smooth"
+            });
+        });
 
-            posicion -= ancho;
-            if (posicion < 0) posicion = maxScroll;
-
-            lista.style.transform = `translateX(-${posicion}px)`;
+        derecha.addEventListener("click", () => {
+            lista.scrollBy({
+                left: 280,
+                behavior: "smooth"
+            });
         });
     });
 });
