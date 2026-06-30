e223f57b1348ed2c28755db4fcaa2037dd51496d
rachelmarro <yajaira.marroquin@catolica.edu.sv>
2026-06-29 20:41:39 -0600

    edite los scripts

--- js/script.js
+++ js/script.js
@@ -4,34 +4,36 @@ carruseles.forEach((carrusel) => {
     const menuLista = carrusel.querySelector(".menu-lista");
     const btnIzquierda = carrusel.querySelector(".flecha-izquierda");
     const btnDerecha = carrusel.querySelector(".flecha-derecha");
+    const ventana = carrusel.querySelector(".carrusel-ventana");
 
     let posicion = 0;
 
-    btnDerecha.addEventListener("click", () => {
+    function mover(direccion) {
         const tarjeta = menuLista.querySelector(".tarjeta");
-        const anchoTarjeta = tarjeta.offsetWidth + 18;
-        const maxScroll = menuLista.scrollWidth - carrusel.querySelector(".carrusel-ventana").offsetWidth;
+        const estilos = getComputedStyle(menuLista);
+        const gap = parseInt(estilos.gap) || 18;
 
-        posicion += anchoTarjeta;
+        const anchoTarjeta = tarjeta.offsetWidth + gap;
+        const maxScroll = menuLista.scrollWidth - ventana.offsetWidth;
+
+        posicion += direccion * anchoTarjeta;
 
         if (posicion > maxScroll) {
             posicion = 0;
         }
-
-        menuLista.style.transform = `translateX(-${posicion}px)`;
-    });
-
-    btnIzquierda.addEventListener("click", () => {
-        const tarjeta = menuLista.querySelector(".tarjeta");
-        const anchoTarjeta = tarjeta.offsetWidth + 18;
-        const maxScroll = menuLista.scrollWidth - carrusel.querySelector(".carrusel-ventana").offsetWidth;
-
-        posicion -= anchoTarjeta;
 
         if (posicion < 0) {
             posicion = maxScroll;
         }
 
         menuLista.style.transform = `translateX(-${posicion}px)`;
+    }
+
+    btnDerecha.addEventListener("click", () => mover(1));
+    btnIzquierda.addEventListener("click", () => mover(-1));
+
+    window.addEventListener("resize", () => {
+        posicion = 0;
+        menuLista.style.transform = "translateX(0)";
     });
 });
